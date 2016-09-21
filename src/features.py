11f1dc852493cebae4dee51aa7dad8c635fc636f
acorbat <agustin.corbat@gmail.com>
2016-09-21 00:45:05 -0300

    Corregido el save y load de pandas

--- src/features.py
+++ src/features.py
@@ -33,7 +33,7 @@ def list_of_headers_load(df):
 def features(df_emails):
     df_columns = ['class']
     try:
-        df = pd.read_csv('trained/features.pandas') # tal vez falta , encoding='utf-8'
+        df = pd.read_pickle('trained/features.pandas') # tal vez falta , encoding='utf-8'
     except IOError:
         df = pd.DataFrame(df_emails, columns=['class'])
     add_feature_len(df_emails, df, df_columns)
@@ -44,7 +44,7 @@ def features(df_emails):
     add_feature_email_headers(emails_by_headers, df, df_columns)
     add_feature_subject_most_common_words_spam(emails_by_headers, df, df_columns)
     add_feature_subject_most_common_words_ham(emails_by_headers, df, df_columns)
-    df.to_pandas('trained/features.pandas') # tal vez falta , encoding='utf-8'
+    df.to_pickle('trained/features.pandas') # tal vez falta , encoding='utf-8'
     #df = df.loc[:, df_columns]
     return df
 
