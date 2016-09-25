96ebd10a3811905b1ebb026a063dfbb5e0331ed8
acorbat <agustin.corbat@gmail.com>
2016-09-25 19:31:16 -0300

    Corrijo test_pca.py para los clasificadores elegidos

--- src/test_pca.py
+++ src/test_pca.py
@@ -16,15 +16,21 @@ Y = df['class']
 Cant_Atributos = len(df.columns) - 1
 alpha = [Cant_Atributos * 0.1, Cant_Atributos * 0.2,  Cant_Atributos * 0.3, Cant_Atributos * 0.4, Cant_Atributos * 0.5, Cant_Atributos * 0.6, Cant_Atributos * 0.7]
 alpha =  [int(round(component)) for component in alpha]
-clasificador = 'KNeighbors'
+
+# Clasificador 1 y sus parametros
+clasificador = 'RandomTrees'
+parameters =  [15, 0.1, 110, 1]
 
 print 'PCA con ' + clasificador
 for component in alpha:
     print 'alpha: ' + str(component)
-    res = cross_validation(X, Y, 'PCA', component, clasificador, 3)
+    res = cross_validation(X, Y, 'PCA', component, clasificador, parameters)
 
+#Clasificador numero 2 y sus parametros
 clasificador = 'DecisionTree'
+parameters = 110
+
 print 'PCA con ' + clasificador
 for component in alpha:
     print 'alpha: ' + str(component)
-    res = cross_validation(X, Y, 'PCA', component, clasificador, 5)
+    res = cross_validation(X, Y, 'PCA', component, clasificador, parameters)
